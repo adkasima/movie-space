a16a5ccc0eea3bbd5ad594b278455df5f91e5638
Adriel <adrielkasima@gmail.com>
2025-04-10 12:23:17 -0300

    :tada: first commit

---
+++ src/main/java/com/moviespace/MoviespaceApplication.java
@@ -0,0 +1,13 @@
+package com.moviespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviespace/MoviespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
