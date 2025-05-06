f25a6938eb7bec48ed8c68b8752e4a37220bf76c
Adriel <adrielkasima@gmail.com>
2025-05-05 21:46:34 -0300

    feat: Create Movie endpoints and services

--- src/main/java/com/moviespace/controller/MovieController.java
+++ src/main/java/com/moviespace/controller/MovieController.java
@@ -1,9 +1,17 @@
 package com.moviespace.controller;
 
+import com.moviespace.controller.request.MovieRequest;
+import com.moviespace.controller.response.MovieResponse;
+import com.moviespace.entity.Movie;
+import com.moviespace.mapper.MovieMapper;
 import com.moviespace.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/moviespace/movie")
@@ -11,4 +19,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieResponse>> getAll() {
+        List<MovieResponse> movies = movieService.findAll()
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList();
+
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> getById(@PathVariable Long id) {
+        return movieService.getById(id)
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        Movie newMovie = MovieMapper.toMovie(request);
+        Movie savedMovie = movieService.save(newMovie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(MovieMapper.toMovieResponse(savedMovie));
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        movieService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
 }
--- src/main/java/com/moviespace/service/MovieService.java
+++ src/main/java/com/moviespace/service/MovieService.java
@@ -1,12 +1,51 @@
 package com.moviespace.service;
 
+import com.moviespace.entity.Category;
+import com.moviespace.entity.Movie;
+import com.moviespace.entity.Streaming;
 import com.moviespace.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreaming(movie.getStreamings()));
+        return movieRepository.save(movie);
+    }
+
+    public void deleteById(Long id) {
+        movieRepository.deleteById(id);
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.getById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreaming(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.getById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
+    }
 }
