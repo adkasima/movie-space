25a5d5ec682c11c6211afd7722a46f83617e3ceb
Adriel <adrielkasima@gmail.com>
2025-05-07 21:34:02 -0300

    feat: Create Spring Security configs to handle with endpoints requests permissions and Bcrypt for passwords

---
+++ src/main/java/com/moviespace/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.moviespace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+//    private final SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/moviespace/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/moviespace/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+//                .addFilter()
+                .build();
+    }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
