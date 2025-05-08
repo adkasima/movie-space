ad549fd76e500cebb476ddce4e0a6cda8fccb1e4
Adriel <adrielkasima@gmail.com>
2025-05-07 21:30:19 -0300

    feat: Create User entity, service, repository and migration for table

---
+++ src/main/java/com/moviespace/entity/User.java
@@ -0,0 +1,29 @@
+package com.moviespace.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
