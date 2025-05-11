87b55b4ce5f9c7ecf3e110a6c616bbe0ab2c41c3
Adriel <adrielkasima@gmail.com>
2025-05-10 22:01:47 -0300

    feat: User implements UserDetails

--- src/main/java/com/moviespace/entity/User.java
+++ src/main/java/com/moviespace/entity/User.java
@@ -1,18 +1,21 @@
 package com.moviespace.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-@Data
-public class User {
+@Getter
+@Setter
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +29,38 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
